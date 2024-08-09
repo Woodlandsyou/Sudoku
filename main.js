54550e86a7a66539d79673416e4343533e435d8f
Woodlandsyou <linus_hahle@web.de>
2024-08-09 22:52:17 +0200

    new Project

---
+++ main.js
@@ -0,0 +1,33 @@
+const vw = window.innerWidth, vh = window.innerHeight;
+
+const sudokuContainer = document.getElementById('sudokuContainer');
+sudokuContainer.style.width = vw <= vh ? vw * 0.9 + 'px':vh * 0.9 + 'px';
+sudokuContainer.style.height = sudokuContainer.style.width;
+
+const cols = 4, rows = cols;
+const _width = sudokuContainer.offsetWidth, _height = sudokuContainer.offsetHeight;
+const s = _width <= _height ? _width / cols:_height / cols;
+
+const sudoku = new Array(cols);
+
+createDivs();
+
+function createDivs() {
+    for (let i = 0; i < cols; i++) {
+        sudoku[i] = new Array(rows);
+        for (let j = 0; j < rows; j++) {
+            createDiv(sudokuContainer, i, j);
+        }
+    }
+}
+
+function createDiv(parent, i, j) {
+    const e = document.createElement('div');
+    const p = document.createElement('p');
+    p.textContent = i * rows + j;
+    e.appendChild(p);
+    e.style.width = s - 1 +'px';
+    e.style.height = s +'px';
+    e.classList.add('item');
+    parent.appendChild(e);
+}
