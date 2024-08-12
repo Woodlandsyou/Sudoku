88ad9d5ce15d35aa0da3c40a777506f8254c4369
Woodlandsyou <linus_hahle@web.de>
2024-08-12 19:15:35 +0200

    added lines

--- main.js
+++ main.js
@@ -14,8 +14,12 @@ class Cell {
         this.x = x;
         this.y = y;
         this.div = createDiv(sudokuContainer, this.x * Math.pow(size, 2) + this.y + 1);
+        this.div.classList.add(`item-modsize-${this.x % size}-x`);
+        if(this.x === Math.pow(size, 2) - 1) this.div.classList.add('last-item-x');
+        if(this.y === Math.pow(size, 2) - 1) this.div.classList.add('last-item-y');
+        this.div.classList.add(`item-modsize-${this.y % size}-y`);
         this.indeces = [];
-        for (let i = 1; i >= Math.pow(size, 2); i++) this.indeces.push(i);
+        for (let i = 1; i <= Math.pow(size, 2); i++) this.indeces.push(i);
         this.number = this.indeces[Math.floor(Math.random() * this.indeces.length)];
     }
 
@@ -35,35 +39,26 @@ function makeArray() {
     for (let i = 0; i < Math.pow(size, 2); i++) {
         sudoku[i] = new Array(Math.pow(size, 2));
         for (let j = 0; j < Math.pow(size, 2); j++) {
-            sudoku[i][j] = new Cell(i, j);
+            sudoku[i][j] = new Cell(j, i);
         }
     }
 }
 
-function createDiv(parent, content) {
+function createDiv(parent) {
     const e = document.createElement('div');
-    e.appendChild(createP(content));
+    const p = document.createElement('p');
+    e.appendChild(p);
     e.style.width = s - 1 +'px';
     e.style.height = s +'px';
     e.classList.add('item');
-
     return parent.appendChild(e);
 }
 
-function createP(content) {
-    const p = document.createElement('p');
-    p.textContent = content;
-
-    p.contentEditable = true;
-    const observer = new MutationObserver(list => {
-        const e = list[0].target;
-        if(e.textContent !== '<empty string>' || parseInt(e.textContent).toString() == 'NaN' || parseInt(e.textContent) < 0 || parseInt(e.textContent) > Math.pow(size, ))
-        console.log(e.textContent);
-    });
-    observer.observe(p, {attributes: false, childList: true, subtree: true});
-    return p;
-}
-
 function fillSudoku() {
-
+    for (let i = 0; i < sudoku.length; i++) {
+        for (let j = 0; j < sudoku[i].length; j++) {
+            const e = sudoku[i][j];
+            
+        }        
+    }
 }
