a0568305744c2024530db421c04e81e92ed7b27f
Woodlandsyou <linus_hahle@web.de>
2025-01-10 16:38:16 +0100

    renamed Iterators and made Iterators into Generator functions

--- Cell.js
+++ Cell.js
@@ -1,9 +1,8 @@
-import { size } from "./main.js";
-import { boxIterator, columnInterator, rowIterator } from "./Interartors.js";
-
+import { grid, size, squareSize } from "./main.js";
 
 export class Cell {
-    constructor() {
+    constructor(x, y) {
+        this.d = {x: x, y: y};
         this.index = (() => {
             let a = [];
             for(let i = 0; i < size; i++) a.push(i + 1);
@@ -11,6 +10,49 @@ export class Cell {
         })();
 
         this.number = this.index[Math.floor(Math.random() * this.index.length)];
+        this.columnInterator();
+        this.rowIterator();
+        // this.boxIterator();
+    }
 
+    columnInterator() {
+        for (let i = 0; size * i + this.d.x < size * size; i++) {
+            const iter = size * i + this.d.x;
+            grid[iter].index.splice(grid[iter].index.indexof(this.number), 1);
+        }
+    }
+
+    rowIterator() {
+        for (let i = 0; size * this.d.y + i < size * (this.d.y + 1); i++) {
+            const iter = size * this.d.y + i;
+            grid[iter].index.splice(grid[iter].index.indexof(this.number), 1);
+        }
+    }
+
+    boxIterator(box) {
+        let f = (() => {
+            let a = [];
+            for (let i = 0; i < squareSize * squareSize - 1; i++) {
+                if(i % squareSize === squareSize - 1) a.push(squareSize * (squareSize - 1) + 1);
+                else a.push(1);
+            }
+            return a;
+        })();
+    
+        for (let i = 0; i < squareSize * squareSize; i++) {
+            let indexInSquare = f.slice(0, i);
+            let indexOfSquare = f.slice(0, box);
+            
+            let sigma = array => {
+                try {
+                    return array.reduce((accu, current) => accu + current)
+                } catch (error) {
+                    return 0;
+                }
+            };
+
+            const iter = sigma(indexInSquare) + squareSize * sigma(indexOfSquare);
+            grid[iter].index.splice(grid[iter].index.indexof(this.number), 1);
+        }
     }
 }
--- Iterators.js
+++ Iterators.js
@@ -1,18 +1,18 @@
 import { size, squareSize } from "./main.js";
 
-export function columnInterator(column) {
+export function* columnInteratorGenerator(column) {
     for (let i = 0; size * i + column < size * size; i++) {
-        console.log(size * i + column);    
+        yield size * i + column;
     }
 }
 
-export function rowIterator(row) {
+export function* rowIteratorGenerator(row) {
     for (let i = 0; size * row + i < size * (row + 1); i++) {
-        console.log(size * row + i);
+        yield size * row + i;
     }
 }
 
-export function boxIterator(box) {
+export function* boxIteratorGenerator(box) {
     let f = (() => {
         let a = [];
         for (let i = 0; i < squareSize * squareSize - 1; i++) {
@@ -33,6 +33,6 @@ export function boxIterator(box) {
                 return 0;
             }
         };
-        console.log(sigma(indexInSquare) + squareSize * sigma(indexOfSquare));
+        yield sigma(indexInSquare) + squareSize * sigma(indexOfSquare);
     }
 }
